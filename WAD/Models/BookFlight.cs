72f95283fa05de6fa18e07666d8f87578d61d754
shirounk01 <raresmihai18@gmail.com>
2023-11-27 23:38:23 +0200

    change try

--- WAD/Models/BookFlight.cs
+++ WAD/Models/BookFlight.cs
@@ -3,12 +3,9 @@
     public class BookFlight
     {
         public int BookFlightId { get; set; }
-        //public int UserId { get; set; }
-        public string UserGuid { get; set; }
+        public string? UserGuid { get; set; }
         public int FlightId { get; set; }
         public DateTime RegistrationDate { get; set; }
-
-        //public User? User { get; set; }
         public Flight? Flight { get; set; }
     }
 }
